7e19d33ed27d9a5407f32380a94bb50ee3480dc7
Anas Merbouh <anas.merbouh@outlook.com>
2019-12-28 23:22:07 -0500

    add ForgotPasswordViewController

---
+++ parts-management-iOS/View Controllers/Authentication/ForgotPasswordViewController.h
@@ -0,0 +1,17 @@
+//
+//  ForgotPasswordViewController.h
+//  parts-management-iOS
+//
+//  Created by Anas Merbouh on 2019-12-28.
+//  Copyright © 2019 Anas Merbouh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ForgotPasswordViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ parts-management-iOS/View Controllers/Authentication/ForgotPasswordViewController.m
@@ -0,0 +1,32 @@
+//
+//  ForgotPasswordViewController.m
+//  parts-management-iOS
+//
+//  Created by Anas Merbouh on 2019-12-28.
+//  Copyright © 2019 Anas Merbouh. All rights reserved.
+//
+
+#import "ForgotPasswordViewController.h"
+
+@interface ForgotPasswordViewController ()
+
+@end
+
+@implementation ForgotPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
